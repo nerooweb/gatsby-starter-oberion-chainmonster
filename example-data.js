50f0a8367b25cd241d19f2e516515e18ec050e3b
Thomas Jan Uta <code@thomasuta.com>
2020-05-05 14:21:38 +0200

    Add example-data

---
+++ example-data.js
@@ -0,0 +1,42 @@
+export default {
+  "status": "success",
+  "gamePage": {
+    "appid": 815490,
+    "gameName": "Chainmonsters",
+    "description": "Chainmonsters is a massively multiplayer RPG inspired by Pokemon.<br><br><br>What started out as a blockchain exclusive collectible game has fully fledged into an immersive MMO with players from around the world competing against each other to become the next world champion.<br><br>The game world is divided across 3 continents that each have their own flair and special history attached. Catch new Chainmons and assemble a team to counter your friends and foes! <br>Compete against other players through Ranked play (Pre-Season available in Early Access) and gain ranks. Every week the best players in the world are assembled to act as the new Furious Five - the legends each player has to defeat before entering the hall of champions!<h2>Story Campaign:</h2>A meteor of some kind has hit the country and brought a mysterious plague with it - as a result the previously happily living Chainmons have evolved and seem to have become aggressive, attacking anyone leaving the safe roads and wandering into the wild. Professor Satoshi has made it her task to find a cure for this ever spreading disease and she needs your help! But you alone won&apos;t be enough - gather your friends, explore the darkest dungeons and find out what this plague is and how to cure it once and for all.<h2>Massive Online Experience:</h2>The game world runs on dedicated game servers that allow thousands of players to battle, explore and interact together in one huge open world. The game dynamically scales the number of players in your area depending on traffic while reserving space specifically for your friends. This way we have a great mix of a believable online multiplayer world while maintaining the beauty and vividness of story relevant missions.<h2>Cross-Platform:</h2>True Crosss-Platform experience being available from day 1(!) on PC/Mac and mobile (with consoles following).<br>We believe that you should be able to have the same great experience no matter what device or platform you are gaming on - assemble your team on the phone in the Metro in the morning, compete against a friend on their console in the afternoon, and do some ranked play on your PC in the evening. Everything is connected.<h2>Quests: </h2>In addition to a streamlined story campaign there are daily, weekly and monthly challenges to be completed either by you or together with other players.<h2>Raids: </h2>As a real MMO we are putting a lot of focus in large community events and one of these examples are public Raids.<br>Ever thought about battling with hundreds of players together to defeat a big boss?<h2>Customization: </h2>Your player is your avatar. Design it any way you like using a huge range of cosmetically items. Looking for a change? Unlock even more items by progressing the story and participate in events to claim exclusive visual rewards!<h2>Battle Mechanic:</h2>The main battle mechanic consists of two players and a team of 6 Chainmons each. Take turns to damage your opponent and try to counter them in any way. Look out for special events and modifiers that heavily alter the battles so you need to adjust to different situations!<h2>Abilities:</h2>Every few levels your Chainmons will learn new abilities. Only 4 abilities can be active at any time. However - you can dynamically change your Chainmons abilities on the fly outside of battles, to surprise your next opponent!",
+    "screenshots": [
+      {
+        "id": 0,
+        "path_thumbnail": "https://steamcdn-a.akamaihd.net/steam/apps/815490/ss_4b7e313889501c0cb7a9be292465eb8c6389236b.600x338.jpg?t=1580383327",
+        "path_full": "https://steamcdn-a.akamaihd.net/steam/apps/815490/ss_4b7e313889501c0cb7a9be292465eb8c6389236b.1920x1080.jpg?t=1580383327"
+      },
+      {
+        "id": 1,
+        "path_thumbnail": "https://steamcdn-a.akamaihd.net/steam/apps/815490/ss_6c058f6d152f15eab0e13150134bd2d159d98bf6.600x338.jpg?t=1580383327",
+        "path_full": "https://steamcdn-a.akamaihd.net/steam/apps/815490/ss_6c058f6d152f15eab0e13150134bd2d159d98bf6.1920x1080.jpg?t=1580383327"
+      },
+      {
+        "id": 2,
+        "path_thumbnail": "https://steamcdn-a.akamaihd.net/steam/apps/815490/ss_26fdfb53de70e98f5e246b71384919551add750e.600x338.jpg?t=1580383327",
+        "path_full": "https://steamcdn-a.akamaihd.net/steam/apps/815490/ss_26fdfb53de70e98f5e246b71384919551add750e.1920x1080.jpg?t=1580383327"
+      },
+      {
+        "id": 3,
+        "path_thumbnail": "https://steamcdn-a.akamaihd.net/steam/apps/815490/ss_fc5685a66d6469830f41e3531122d79928d16087.600x338.jpg?t=1580383327",
+        "path_full": "https://steamcdn-a.akamaihd.net/steam/apps/815490/ss_fc5685a66d6469830f41e3531122d79928d16087.1920x1080.jpg?t=1580383327"
+      },
+      {
+        "id": 4,
+        "path_thumbnail": "https://steamcdn-a.akamaihd.net/steam/apps/815490/ss_c9900c8590733b03de1a18dd75d152e7e7924fef.600x338.jpg?t=1580383327",
+        "path_full": "https://steamcdn-a.akamaihd.net/steam/apps/815490/ss_c9900c8590733b03de1a18dd75d152e7e7924fef.1920x1080.jpg?t=1580383327"
+      }
+    ],
+    "movies": null,
+    "packageid": null,
+    "legalNotice": "Copyright B-Side Games 2019",
+    "supportInfo": {
+      "url": "https://playchainmonsters.com",
+      "email": "support@bsidegames.com"
+    }
+  }
+}
